e8717952a08f08dcacee131b3a47b71c4745391c
Thomas (T.J.) Dokas <tjdokas@gmail.com>
2023-08-19 00:12:43 -0400

    Initial Commit

---
+++ Stethoscope Watch App/StethoscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StethoscopeApp.swift
+//  Stethoscope Watch App
+//
+//  Created by TJ Dokas on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Stethoscope_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
