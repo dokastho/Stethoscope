dc6759b19762f86090fc68f06ef4c632f06e8cd6
Thomas (T.J.) Dokas <tjdokas@gmail.com>
2023-08-19 00:51:21 -0400

    Add basic request code

--- Stethoscope Watch App/StethoscopeApp.swift
+++ Stethoscope Watch App/StethoscopeApp.swift
@@ -7,11 +7,39 @@
 
 import SwiftUI
 
+enum STATUS {
+case UP
+case DOWN
+case LOADING
+}
+
+struct ServerStatus {
+    var status: STATUS
+}
+
 @main
 struct Stethoscope_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    func get_status() -> ServerStatus {
+        var stat = ServerStatus(status: STATUS.LOADING)
+        
+        let url = URL(string: "https://dokasfam.com:4221")!
+        
+        let task = URLSession.shared.dataTask(with: url) {data, response, error in
+            if let data = data {
+                print("success")
+            } else if let error = error {
+                print("fail: \(error)")
+            }
+        }
+        
+        task.resume()
+        
+        return stat
     }
 }
